380880bb92d5dacedd214055b70305a65d5760f1
Filippo Floramo <filifloramo@gmail.com>
2023-02-13 20:05:42 +0100

    first init

---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const port = 3000;
+
+
+const app = express();
+
+// register view engine
+//app.set('views', 'views');
+
+
+//creating middleware & static files
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+   return res.status(200).json({
+      title: "Express Testing",
+      message: "The app is working properly!",
+   })
+})
+
+app.listen(port, () => {
+   console.log("ready on port", port);
+})
