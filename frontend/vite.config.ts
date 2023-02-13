2779f907e926d5f88fadbbbb4864aa0cfdbfa575
Filippo Floramo <filifloramo@gmail.com>
2023-02-13 22:10:12 +0100

    init

---
+++ frontend/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+import eslint from 'vite-plugin-eslint';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [preact(), eslint()],
+})
